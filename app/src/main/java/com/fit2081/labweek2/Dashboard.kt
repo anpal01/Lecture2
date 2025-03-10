409880bed784a5e65ade71a483c7274343041204
anpal <anpalatino@gmail.com>
2025-03-10 19:54:36 +1100

    added bottom bar functionality

--- app/src/main/java/com/fit2081/labweek2/Dashboard.kt
+++ app/src/main/java/com/fit2081/labweek2/Dashboard.kt
@@ -7,7 +7,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,6 +25,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.fit2081.labweek2.ui.theme.LabWeek2Theme
 
@@ -44,6 +55,7 @@ class Dashboard : ComponentActivity() {
                         )
 
                     }
+                    MyBottomBar()
                 }
             }
         }
@@ -51,17 +63,36 @@ class Dashboard : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LabWeek2Theme {
-        Greeting("Android")
+fun MyBottomBar() {
+    Scaffold(
+        bottomBar = {
+            BottomAppBar(
+                modifier = Modifier.height(60.dp),
+                content = {
+                    IconButton(onClick = { /* TODO: Add action */ }) {
+                        Icon(Icons.Filled.Check, contentDescription = "Check icon")
+                    }
+                    IconButton(onClick = { /* TODO: Navigate to another screen */ }) {
+                        Icon(Icons.Filled.Home, contentDescription = "Go Home")
+                    }
+                    Button(onClick = { /* TODO: Add button action */ }) {
+                        Text("Click Me")
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /* TODO: Add FAB action */ }) {
+                Icon(Icons.Filled.Add, contentDescription = "Add something")
+            }
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding).fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Hello, Bottom Bar!", fontSize = 24.sp)
+        }
     }
 }
+
