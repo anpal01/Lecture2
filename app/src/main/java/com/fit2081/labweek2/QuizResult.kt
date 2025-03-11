262a319db78d632573c47a81a41f6a41dbf1f26d
anpal <anpalatino@gmail.com>
2025-03-11 17:33:28 +1100

    added result processing depending on the user's quiz result

--- app/src/main/java/com/fit2081/labweek2/QuizResult.kt
+++ app/src/main/java/com/fit2081/labweek2/QuizResult.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
@@ -16,7 +19,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.fit2081.labweek2.ui.theme.LabWeek2Theme
 
 class QuizResult : ComponentActivity() {
@@ -24,6 +29,10 @@ class QuizResult : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+
+            //retrieves the passed on value from quiz_page
+            val isPassed = intent.getBooleanExtra("isPassed", false)
+
             LabWeek2Theme {
                 Scaffold(
                     topBar = { TopBar() }, // Include Top Bar
@@ -38,12 +47,24 @@ class QuizResult : ComponentActivity() {
                         modifier = Modifier
                             .padding(innerPadding)
                             .fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
                     ) {
-                        //add content here
+                        if (isPassed) {
+                            Image(
+                                painter = painterResource(R.drawable.green_tick),
+                                contentDescription = "Passed",
+                                modifier = Modifier.size(100.dp)
+                            )
+                        } else {
+                            Image(
+                                painter = painterResource(R.drawable.red_corss),
+                                contentDescription = "Failed",
+                                modifier = Modifier.size(100.dp)
+                            )
+                        }
                     }
                 }
-
             }
         }
     }
