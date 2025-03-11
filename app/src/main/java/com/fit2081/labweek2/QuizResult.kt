b1086af173f0d7a908bd1aa6d25e2a9870863d94
anpal <anpalatino@gmail.com>
2025-03-11 17:27:29 +1100

    added scaffold template to quizresult activity

--- app/src/main/java/com/fit2081/labweek2/QuizResult.kt
+++ app/src/main/java/com/fit2081/labweek2/QuizResult.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.fit2081.labweek2.ui.theme.LabWeek2Theme
@@ -19,7 +25,24 @@ class QuizResult : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LabWeek2Theme {
-                DashboardScreen()
+                Scaffold(
+                    topBar = { TopBar() }, // Include Top Bar
+                    bottomBar = { MyBottomBar() }, // Include Bottom Bar
+                    floatingActionButton = {
+                        FloatingActionButton(onClick = { /* TODO: Add FAB action */ }) {
+                            Icon(Icons.Filled.Add, contentDescription = "Add something")
+                        }
+                    }
+                ) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        //add content here
+                    }
+                }
 
             }
         }
