817a460f47e8ad700865c50befb64ca6dc0d9b3a
anpal <anpalatino@gmail.com>
2025-03-11 17:25:51 +1100

    refactored component activity in quiz_page so that scaffold formats the top, bottom bars and content as one

--- app/src/main/java/com/fit2081/labweek2/Quiz_Page.kt
+++ app/src/main/java/com/fit2081/labweek2/Quiz_Page.kt
@@ -13,8 +13,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -35,10 +39,21 @@ class Quiz_Page : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LabWeek2Theme {
-                Scaffold (modifier = Modifier.fillMaxSize()){ innerPadding ->
-                    Column (modifier = Modifier.padding(innerPadding).fillMaxSize(),
+                Scaffold(
+                    topBar = { TopBar() }, // Include Top Bar
+                    bottomBar = { MyBottomBar() }, // Include Bottom Bar
+                    floatingActionButton = {
+                        FloatingActionButton(onClick = { /* TODO: Add FAB action */ }) {
+                            Icon(Icons.Filled.Add, contentDescription = "Add something")
+                        }
+                    }
+                ) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize(),
                         horizontalAlignment = Alignment.CenterHorizontally
-                    ){
+                    ) {
                         Image(
                             painter = painterResource(id = R.drawable.fit2081_logo_yellow),
                             contentDescription = "Quiz Image",
@@ -46,6 +61,7 @@ class Quiz_Page : ComponentActivity() {
                                 .fillMaxWidth()
                                 .height(200.dp)
                                 .padding(16.dp)
+
                         )
 
                         //since values can be changed continuously throughout process, theyre mutable
@@ -86,10 +102,8 @@ class Quiz_Page : ComponentActivity() {
                         }) {
                             Text("I'm Done!")
                         }
-
                     }
                 }
-
             }
         }
     }
