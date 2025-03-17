a68a7cecba0c215e7bee847ed74c6933fe2b4fa0
anpal <anpalatino@gmail.com>
2025-03-17 20:30:57 +1100

    added date and time picker methods

--- app/src/main/java/com/fit2081/labweek2/week3.kt
+++ app/src/main/java/com/fit2081/labweek2/week3.kt
@@ -1,6 +1,11 @@
 package com.fit2081.labweek2
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.icu.util.Calendar
 import android.os.Bundle
+import android.provider.CalendarContract.Calendars
+import android.widget.DatePicker
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -9,6 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -34,7 +40,7 @@ class week3 : ComponentActivity() {
 
             //variables that retrieve timepicker dialogs
             //var mTimePickerDialog = TimePickerFun(mTime)
-            //var DatePickerDialog = DatePickerFun(mDate)
+            var DatePickerDialog = DatePickerFun(mDate)
 
             var sliderValue by remember { mutableStateOf(10f) }
 
@@ -59,10 +65,69 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LabWeek2Theme {
-        Greeting("Android")
-    }
+fun DatePickerFun(mDate: MutableState<String>): DatePickerDialog { //the parameters and the return value after colon
+
+    //get current context
+    val mContext = LocalContext.current
+
+    //variables to hold date info
+    val mYear: Int
+    val mMonth: Int
+    val mDay: Int //why the colon? rather than =?
+
+    //get calendar instance
+    val mCalendar = Calendar.getInstance()
+
+    //take date from this calendar
+    mYear = mCalendar.get(Calendar.YEAR)
+    mMonth = mCalendar.get(Calendar.MONTH)
+    mDay = mCalendar.get(Calendar.DAY_OF_MONTH)
+
+    //return and create datepickerdialog
+
+    return DatePickerDialog(
+        //we're returning a context for todays date
+        //this is called whenever the dates being set
+        //provides the current date
+
+        mContext,
+        {
+            _: DatePicker, mYear: Int, mMonth: Int, mDayOfMonth: Int ->
+            mDate.value = "$mDayOfMonth/${mMonth + 1}/$mYear"
+        }, mYear, mMonth, mDay
+    )
+}
+
+
+@Composable
+fun TimePickerFun(mTime: MutableState<String>): TimePickerDialog { //the parameters and the return value after colon
+
+    //get current context
+    val mContext = LocalContext.current
+
+    //get calendar instance
+    val mCalendar = Calendar.getInstance()
+
+    //get current hour and minute
+    val mHour = mCalendar.get(Calendar.HOUR_OF_DAY)
+    val mMinute = mCalendar.get(Calendar.MINUTE)
+    //val mSecond = mCalendar.get(Calendar.SECOND), can add this if want to include minutes
+
+    //set to current time for calendar
+    mCalendar.time = Calendar.getInstance().time
+
+    //return and create timepickerdialog
+
+    return TimePickerDialog(
+        //we're returning a context for todays time
+        //this is called whenever the dates being set
+        //provides the current date
+
+        mContext,
+        {
+                _, mHour: Int, mMinute: Int -> //this is where the code listens for changes
+            mTime.value = "$mHour:$mMinute" //listened values are than put into the initial hour and minute
+        }, mHour, mMinute, false //use 24hr time or not
+    )
 }
