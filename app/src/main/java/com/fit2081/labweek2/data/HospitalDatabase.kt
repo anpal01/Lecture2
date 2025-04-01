bcf1681d54ea14bff1b84327482551372e9c4504
anpal <anpalatino@gmail.com>
2025-04-01 15:52:41 +1100

    added hospital db code

--- app/src/main/java/com/fit2081/labweek2/data/HospitalDatabase.kt
+++ app/src/main/java/com/fit2081/labweek2/data/HospitalDatabase.kt
@@ -1,3 +1,36 @@
 package com.fit2081.labweek2.data
 
-data class HospitalDatabase()
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+
+@Database(entities = [Patient::class], version = 1, exportSchema = false)
+//let it know its a room db
+abstract class HospitalDatabase: RoomDatabase() {
+
+    //returns dao, room does this
+
+    abstract fun patientDao(): PatientDao
+
+    companion object {
+
+        //holds db instance, and volatile, visible to all threads?
+        @Volatile
+        private var Instance: HospitalDatabase? = null
+
+        //returns db instance and if null, creates one
+
+        fun getDatabase(context: Context): HospitalDatabase {
+
+            //synchronised = one thread can access at a time
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, HospitalDatabase::class.java, "hospital_database")
+                    .build().also { Instance = it }
+            }
+        }
+
+    }
+
+}
