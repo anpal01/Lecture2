bb17f1db16eb55a958bbf81b3db727ab67fcb1fa
anpal <anpalatino@gmail.com>
2025-04-01 16:11:30 +1100

    added view model functionality for hospital db

--- app/src/main/java/com/fit2081/labweek2/data/PatientViewModel.kt
+++ app/src/main/java/com/fit2081/labweek2/data/PatientViewModel.kt
@@ -1,3 +1,43 @@
 package com.fit2081.labweek2.data
 
-data class PatientViewModel()
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+
+class PatientViewModel(context: Context) : ViewModel() {
+
+    //create repo instance to talk to db
+    private val patientRepo = PatientsRepository(context)
+
+    //provides all patients that are currently stored
+    val allPatients: Flow<List<Patient>> = patientRepo.getAllPatients()
+
+    //viewmodel version of the db methods, gives it to viewmodel
+    fun insert(patient: Patient) = viewModelScope.launch {
+        patientRepo.insert(patient)
+    }
+
+    fun delete(patient: Patient) = viewModelScope.launch {
+        patientRepo.delete(patient)
+    }
+    fun update(patient: Patient) = viewModelScope.launch {
+        patientRepo.update(patient)
+    }
+    fun deleteAllPatients() = viewModelScope.launch {
+        patientRepo.deleteAllPatients()
+    }
+
+    //sets context for view models
+    class PatientViewModelFactory(context: Context) : ViewModelProvider.Factory {
+        private val context = context.applicationContext
+
+        override fun <T : ViewModel> create(modelClass: Class<T>): T =
+            PatientViewModel(context) as T
+
+    }
+
+
+}
