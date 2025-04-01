c9763f8b0470456a0128a1f2a8d47834077c4aa3
anpal <anpalatino@gmail.com>
2025-04-01 15:44:12 +1100

    added patient entity file and code

--- app/src/main/java/com/fit2081/labweek2/data/Patient.kt
+++ app/src/main/java/com/fit2081/labweek2/data/Patient.kt
@@ -1,3 +1,18 @@
 package com.fit2081.labweek2.data
 
-data class Patient()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "patients")
+data class Patient(
+
+    //id for the object, auto made
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+
+    val name: String,
+    val age: Int,
+    val address: String
+
+)
