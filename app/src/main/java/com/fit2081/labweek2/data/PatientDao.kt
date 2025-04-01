76954e41a760ec96e34a15d19e1acf3b7c957262
anpal <anpalatino@gmail.com>
2025-04-01 15:57:40 +1100

    added dao functionality

--- app/src/main/java/com/fit2081/labweek2/data/PatientDao.kt
+++ app/src/main/java/com/fit2081/labweek2/data/PatientDao.kt
@@ -1,4 +1,32 @@
 package com.fit2081.labweek2.data
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface PatientDao {
+
+    //all of the retrieval functions/functions that deal with taking, leaving and updating data
+
+    //suspend is a coroutine function, using suspent at start of methods shows that the functions called from a coroutine
+
+    @Insert
+    suspend fun insert(patient: Patient)
+
+    @Update
+    suspend fun update(patient: Patient)
+
+    @Delete
+    suspend fun delete(patient: Patient)
+
+    @Query("DELETE FROM patients")//means that the function does the same as this sql query
+    suspend fun deleteAllPatients()
+
+    @Query("SELECT * FROM patients ORDER BY id ASC")
+    fun getAllPatients(): Flow<List<Patient>>
+
 }
